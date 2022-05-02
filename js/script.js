4a1bb4c36359e0d014a6c099eca6725d8a6b2819
Matheus Henrique <matheus.hpp135@gmail.com>
2022-05-02 12:23:05 -0300

    add script

--- js/script.js
+++ js/script.js
@@ -8,15 +8,36 @@ function binToDec(binary) {
   return decimal;
 }
 
+function validate(input) {
+  const valid = /^[0-1]+$/;
+  return valid.test(input);
+}
+
 const binInput = document.getElementById("bin");
 const decOutput = document.getElementById("dec");
 const clearButton = document.getElementById("btn-clear");
 
 binInput.addEventListener("input", () => {
-  decOutput.value = binToDec(binInput.value);
+  if (validate(binInput.value)) {
+    if (decOutput.style.color == "red") {
+      decOutput.style.color = "inherit";
+    }
+    decOutput.value = binToDec(binInput.value);
+  } else {
+    if (binInput.value == "") {
+      decOutput.value = "";
+      decOutput.style.color = "inherit";
+    } else {
+      decOutput.value = "Must be 0 or 1";
+      decOutput.style.color = "red";
+    }
+  }
 });
 
-clearButton.addEventListener("click", () => { 
+clearButton.addEventListener("click", () => {
+  if (decOutput.style.color == "red") {
+    decOutput.style.color = "inherit";
+  }
   binInput.value = "";
   decOutput.value = "";
 });
